4f64cd484401476d450c383eb0969e6e6e6955e4
Victor Velasquez <victorv24@gmail.com>
2018-07-30 09:00:14 -0700

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import AlbumsView from './AlbumsView'
-import EachAlbum from './EachAlbum'
-import EachPhoto from './EachPhoto'
+
 
 class App extends Component {
   render() {
@@ -12,8 +11,6 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={AlbumsView} />
-            <Route path="/albums/:id" component={EachAlbum} />
-            <Route path="/photos/:id" component={EachPhoto} />
           </Switch>
         </div>
       </Router>
