fb4275f6ffb3e0159c9fe9155503b0742cd56318
Victor Velasquez <victorv24@gmail.com>
2018-08-02 22:46:51 -0700

    added additional dependencies

--- client/src/actions/albumFunctions.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export function getAlbums() {
-	return axios.get('http://localhost:3001/albums')
-}
-
-export function getAlbum(id) {
-	return axios.get(`http://localhost:3001/albums/${id}?_embed=photos`)
-}
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-import Albums from './Albums'
-import SingleAlbum from './SingleAlbum'
-import SinglePhoto from './SinglePhoto'
+import {getAlbums} from '../actions/albumFunctions.js'
 
 class App extends Component {
-
+  componentDidMount() {
+    getAlbums().then()
+  }
   render() {
     return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Albums} />
-            <Route path="/album/:id" component={SingleAlbum} />
-            <Route path="/photo/:id" component={SinglePhoto} />
-          </Switch>
-        </div>
-      </Router>
+      
     )
   }
 }
